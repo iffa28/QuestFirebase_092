f9de1db30121d65bad98476912cf0291683cde68
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-13 12:47:32 +0700

    menambahkan data class untuk validasi error

--- app/src/main/java/com/example/project_firebase/ui/viewModel/InsertViewMode.kt
+++ app/src/main/java/com/example/project_firebase/ui/viewModel/InsertViewMode.kt
@@ -2,6 +2,20 @@ package com.example.project_firebase.ui.viewModel
 
 import com.example.project_firebase.model.Mahasiswa
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val alamat: String? = null,
+    val jenis_kelamin: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && alamat == null &&
+                jenis_kelamin == null && kelas == null && angkatan == null
+    }
+}
+
 fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
     nim = nim,
     nama = nama,
