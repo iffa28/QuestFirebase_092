2a08e9dbec2e1fbd4d6f961af2c01623f1f41814
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-13 12:22:48 +0700

    menambahkan operasi operasi pada mahasiswaRepository

--- app/src/main/java/com/example/project_firebase/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/project_firebase/repository/MahasiswaRepository.kt
@@ -6,5 +6,8 @@ import kotlinx.coroutines.flow.Flow
 interface MahasiswaRepository {
     suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
-    suspend fun deleteMahasiswa(nim: String)
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasis(mahasiswa: Mahasiswa)
+    suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa>
+
 }
