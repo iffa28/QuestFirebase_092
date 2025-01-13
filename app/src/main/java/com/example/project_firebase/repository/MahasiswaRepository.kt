b085bd1f6a34d9070ff0536dadc81c79de02c856
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-13 12:06:00 +0700

    menambahkan operasi insert dan delete pada mahasiswa Repository

--- app/src/main/java/com/example/project_firebase/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/project_firebase/repository/MahasiswaRepository.kt
@@ -5,4 +5,6 @@ import kotlinx.coroutines.flow.Flow
 
 interface MahasiswaRepository {
     suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun deleteMahasiswa(nim: String)
 }
