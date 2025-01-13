299bd233cfba9e1f79a62d4d6d19bf86137d0525
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-13 12:41:01 +0700

    menambahkan data class variabel untuk menyimpan  data input form

---
+++ app/src/main/java/com/example/project_firebase/ui/viewModel/InsertViewMode.kt
@@ -0,0 +1,10 @@
+package com.example.project_firebase.ui.viewModel
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
