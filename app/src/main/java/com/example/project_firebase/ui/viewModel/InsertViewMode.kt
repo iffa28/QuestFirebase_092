be788d6df469b29b7fcf8ac203f7625bbf9676c3
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-13 12:43:39 +0700

    menambahkan fungsi untuk menyimpan input form ke entity

--- app/src/main/java/com/example/project_firebase/ui/viewModel/InsertViewMode.kt
+++ app/src/main/java/com/example/project_firebase/ui/viewModel/InsertViewMode.kt
@@ -1,10 +1,21 @@
 package com.example.project_firebase.ui.viewModel
 
+import com.example.project_firebase.model.Mahasiswa
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenis_kelamin = jenis_kelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 data class MahasiswaEvent(
     val nim: String = "",
     val nama: String = "",
     val alamat: String = "",
-    val jenisKelamin: String = "",
+    val jenis_kelamin: String = "",
     val kelas: String = "",
     val angkatan: String = ""
 )
